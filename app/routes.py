fbccc5cd642244d4a7893f19bedef2d237d1ce1b
haikudoggies <haikudoggies@gmail.com>
2022-06-29 21:01:03 -0700

    complete mess, probably going to change whole thing later

--- app/routes.py
+++ app/routes.py
@@ -20,3 +20,8 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
