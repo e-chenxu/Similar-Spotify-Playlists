98dec7f3862368df1af3ce21b1da37486ed3c944
haikudoggies <haikudoggies@gmail.com>
2022-07-04 22:38:08 -0700

    textbox

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+def emptyCheck():
+    return
+
+@app.route('/',methods =["GET", "POST"])
 def index():
+    if request.method == "POST":
+       emptyCheck()
+       # getting input with name = fname in HTML form
+       spotify_link = request.form.get("link")
+       return render_template("index.html", spotify_link = spotify_link)
     return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
