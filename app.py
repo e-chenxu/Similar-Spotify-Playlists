a03859a4890bd3237fbaceccdec872cac85c0644
Eric <haikudoggies@gmail.com>
2022-08-29 16:52:31 -0700

    added checkboxes for search algorithm (algorithm not done yet)

--- app.py
+++ app.py
@@ -9,10 +9,13 @@ def index():
     if request.method == "POST":
         # getting input with name = fname in HTML form
         spotify_link = request.form.get("link")
+        search_value = request.form.get("box")
+        print(spotify_link, search_value)
 
         # check if the input is correct, if it isnt, dont do anything
         if not data.correct_link_check(spotify_link):
             return '', 204
+        print('This is standard output')
 
         # now that we have the the spotify playlist link, we have to find 5 similar playlists
 
